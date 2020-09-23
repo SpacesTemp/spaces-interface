dfe9b7ef8fb6f5cd185eb6437b7a9e7481847c19
Shadab Khan <shadab.2005@gmail.com>
2020-09-23 12:31:22 +0530

    Add chat reducer

--- src/components/Channel.tsx
+++ src/components/Channel.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useReducer } from "react";
 import { useParams } from "react-router-dom";
 import styled from "styled-components";
 import dayjs from "dayjs";
@@ -7,6 +7,7 @@ const Main = styled.div`
   padding: 10px;
   background-color: ${({ theme }) => theme.colors.black};
   flex: 1;
+  height: 100%;
 
   & > h1 {
     color: ${({ theme }) => theme.colors.primaryTextColor};
@@ -14,11 +15,12 @@ const Main = styled.div`
   }
 `;
 
-const Chat = styled.div`
+const ChatDiv = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: flex-end;
   height: calc(100% - 58px);
+  overflow: auto;
 `;
 
 const Chatbox = styled.div`
@@ -68,6 +70,27 @@ const ChatInput = styled.input`
   border: 1px solid ${({ theme }) => theme.colors.white};
 `;
 
+type Chat = {
+  user: string;
+  message: string;
+  isCurrentUser: boolean;
+  timestamp: any;
+};
+
+const chatReducer = (
+  state: { chats: Array<Chat> },
+  action: { type: string; message: Chat }
+) => {
+  console.log(action);
+  const newState = { ...state };
+  switch (action.type) {
+    case "ADD_CHAT":
+      newState.chats.push(action.message);
+      return newState;
+  }
+  return state;
+};
+
 const Channel = () => {
   const { id } = useParams();
   const chats = [
@@ -97,11 +120,13 @@ const Channel = () => {
     },
   ];
 
+  const [state, dispatch] = useReducer(chatReducer, { chats });
+
   return (
     <Main>
       <h1>Channel {id}</h1>
-      <Chat>
-        {chats.map((chat) => {
+      <ChatDiv>
+        {state.chats.map((chat: Chat) => {
           return (
             <Chatbox key={chat.timestamp}>
               <Image />
@@ -117,8 +142,28 @@ const Channel = () => {
             </Chatbox>
           );
         })}
-        <ChatInput placeholder="Write your message here..." />
-      </Chat>
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            dispatch({
+              type: "ADD_CHAT",
+              message: {
+                user: "User1",
+                message: (e.target as HTMLFormElement).message.value,
+                isCurrentUser: true,
+                timestamp: Date.now(),
+              },
+            });
+            (e.target as HTMLFormElement).reset();
+          }}
+        >
+          <ChatInput
+            name="message"
+            type="text"
+            placeholder="Write your message here..."
+          />
+        </form>
+      </ChatDiv>
     </Main>
   );
 };
