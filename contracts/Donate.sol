954beceabdc79f4be3b2812145475fc0160b5bd6
dmarzzz <danmarzec@icloud.com>
2020-09-30 14:05:00 -0400

    working donation function

--- contracts/Donate.sol
+++ contracts/Donate.sol
@@ -17,9 +17,18 @@ contract Donate is ERC20, ERC20Detailed, ERC20Mintable, ERC20Pausable, Ownable {
     constructor(string memory _name, string memory _symbol, uint8 _decimals)
         ERC20Detailed(_name, _symbol, _decimals)
         public {}
+
+		function mint(address sender, uint value) public returns (bool) {
+			_mint(sender, value);
+		}
+
+		function () external payable{
+			mint(msg.sender , msg.value);
+		}
 }
 
 
+//crowdsale is outdated so not using anymore, just here for implementation reference purposes
 contract DonateCrowdsale is Crowdsale, MintedCrowdsale {
 
     // Track investor contributions
@@ -30,47 +39,32 @@ contract DonateCrowdsale is Crowdsale, MintedCrowdsale {
     constructor(
         uint256 _rate,
         address payable _wallet,
-        ERC20 _token
+        IERC20 _token
     ) 
-    Crowdsale(_rate, _wallet, _token) public {}
+    Crowdsale(_rate, _wallet, _token) public {
+	}
+
 }
-// contract MyCrowdsale is Crowdsale, MintedCrowdsale {
-//     constructor(
-//         uint256 rate,    // rate in TKNbits
-//         address payable wallet,
-//         IERC20 token
-//     )
-//         MintedCrowdsale()
-//         Crowdsale(rate, wallet, token)
-//         public
-//     {
 
-//     }
-// }
+//reference to deploying a token and crowd sale together
+contract MyCrowdsaleDeployer {
+    constructor(string memory _name, string memory _symbol, uint8 _decimals )
+        public
+    {
+        // create a mintable token
+        ERC20Mintable token = new Donate(_name, _symbol, _decimals);
 
-// contract MyCrowdsaleDeployer {
-//     constructor()
-//         public
-//     {
-//         // create a mintable token
-//         ERC20Mintable token = new Donate();
-
-//         // create the crowdsale and tell it about the token
-//         Crowdsale crowdsale = new MyCrowdsale(
-//             1,               // rate, still in TKNbits
-//             msg.sender,      // send Ether to the deployer
-//             token            // the token
-//         );
-//         // transfer the minter role from this contract (the default)
-//         // to the crowdsale, so it can mint tokens
-//         token.addMinter(address(crowdsale));
-//         token.renounceMinter();
+        // create the crowdsale
+        DonateCrowdsale crowdsale = new DonateCrowdsale(
+            1,               // rate
+            msg.sender,      // send Ether to the deployer address
+            token            // the token
+        );
+        // transfer the minter role from this contract to crowdsale contract
+        token.addMinter(address(crowdsale));
+        token.renounceMinter();
 
 
-//     }
+    }
 
-// 	// function getAmountRaised() public view returns (uint256){
-// 	// 	uint256 amountRaised = address(this).crowdsale.weiRaised(); 
-// 	// 	return amountRaised;
-// 	// }
-// }
+}
--- test/Donate.js
+++ test/Donate.js
@@ -17,39 +17,66 @@ describe("Donate contract", function() {
 
     await donateDeployer.deployed();
 
-    const balance = await donateDeployer.balanceOf(owner.getAddress());
-    console.log(parseInt(balance));
+    const balanceB4 = await donateDeployer.balanceOf(owner.getAddress());
+    console.log(parseInt(balanceB4));
     const name = await donateDeployer.name();
     console.log(name);
     const symbol = await donateDeployer.symbol();
     console.log(symbol);
-    const totalSupply = await donateDeployer.totalSupply();
-    console.log(parseInt(totalSupply));
-  
+    const totalSupplyB4 = await donateDeployer.totalSupply();
+    console.log(parseInt(totalSupplyB4));
+
+    await donateDeployer.mint(addr1.getAddress(), 1)
     
-    const _rate = 500;
-    const _wallet = await owner.getAddress();
-    const _tokenAddress = await donateDeployer.address;
+    const balanceAfter = await donateDeployer.balanceOf(owner.getAddress());
+    console.log(balanceAfter);
+    const totalSupplyAfter = await donateDeployer.totalSupply();
+    console.log(parseInt(totalSupplyAfter));
 
-    console.log(_tokenAddress)
-
-    const Crowdsale = await ethers.getContractFactory("DonateCrowdsale")
-    const CrowdsaleDeployer = await Crowdsale.deploy(_rate , _wallet, _tokenAddress);
-    await CrowdsaleDeployer.deployed();
-
-    const crowdsaleAddress = await CrowdsaleDeployer.address;
-
-    const weiAmount = web3.utils.toWei('1', 'ether')
-
-    await donateDeployer.transferOwnership(crowdsaleAddress);
+    const weiAmount = web3.utils.toHex( web3.utils.toWei('1', 'ether'))
 
 
-   
-    //await donateDeployer.connect(owner).transfer(await addr1.getAddress(), 10);
+    const transaction = {
+      nonce: 2,
+      to: donateDeployer.address,
+      value: weiAmount
 
-    //await Crowdsale.sendTransaction({ value: weiAmount, from: addr1 });
+  };
+  
+    //const singedTransaction = owner.sign(transaction)
+
+    await owner.sendTransaction(transaction);
 
 
+    const balanceAfterTx = await donateDeployer.balanceOf(owner.getAddress());
+    console.log("Balance After Tx" , parseInt(balanceAfterTx));
+    const totalSupplyAfterTx = await donateDeployer.totalSupply();
+    console.log("Total Supply After Tx" , parseInt(totalSupplyAfterTx));
+
+
+
+    // const _rate = 500;
+    // const _wallet = await owner.getAddress();
+    // const _tokenAddress = await donateDeployer.address;
+
+    // console.log(_tokenAddress)
+
+    // const Crowdsale = await ethers.getContractFactory("DonateCrowdsale")
+    // const CrowdsaleDeployer = await Crowdsale.deploy(_rate , _wallet, _tokenAddress);
+    // await CrowdsaleDeployer.deployed();
+
+    // const crowdsaleAddress = await CrowdsaleDeployer.address;
+
+    // const weiAmount = web3.utils.toWei('1', 'ether')
+
+    // await donateDeployer.transferOwnership(crowdsaleAddress);
+
+
+
+    // const crowdsale = await ethers.getContractFactory("MyCrowdsaleDeployer");
+    // const CrowdsaleDeployer = await crowdsale.deploy(_name, _symbol, _decimals);
+    // await CrowdsaleDeployer.deployed();
+    // console.log(CrowdsaleDeployer.address);
     
   });
 });
