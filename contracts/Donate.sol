a010243450bb6be53ac6700f133fe1bd13dd91a3
dmarzzz <danmarzec@icloud.com>
2020-09-30 00:12:40 -0400

    assemblage of random solidity + testing, work in progress

--- contracts/Donate.sol
+++ contracts/Donate.sol
@@ -1,14 +1,76 @@
-pragma solidity ^0.6.0;
+pragma solidity ^0.5.5;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20Detailed.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20Mintable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20Pausable.sol";
+import "@openzeppelin/contracts/ownership/Ownable.sol";
 
-contract Donation is ERC721{
-	using Counters for Counters.Counter;
-     Counters.Counter private _id;
-    address public owner;
+import "@openzeppelin/contracts/crowdsale/emission/MintedCrowdsale.sol";
+import "@openzeppelin/contracts/crowdsale/Crowdsale.sol";
 
-    constructor() ERC721("Gem" , "GEM") public {
-        owner = msg.sender;
-    }
+
+//using openZeplin version2.5 for prototyping b/c it supports crowdsale:
+//https://github.com/OpenZeppelin/openzeppelin-contracts/tree/v2.5.0/contracts/token/ERC20
+
+contract Donate is ERC20, ERC20Detailed, ERC20Mintable, ERC20Pausable, Ownable {
+    constructor(string memory _name, string memory _symbol, uint8 _decimals)
+        ERC20Detailed(_name, _symbol, _decimals)
+        public {}
 }
+
+
+contract DonateCrowdsale is Crowdsale, MintedCrowdsale {
+
+    // Track investor contributions
+    uint256 public investorMinCap = 20000000000000000; // 0.02 ether
+    uint256 public investorHardCap = 50000000000000000000; // 50 ether
+    mapping(address => uint256) public contributions;
+
+    constructor(
+        uint256 _rate,
+        address payable _wallet,
+        ERC20 _token
+    ) 
+    Crowdsale(_rate, _wallet, _token) public {}
+}
+// contract MyCrowdsale is Crowdsale, MintedCrowdsale {
+//     constructor(
+//         uint256 rate,    // rate in TKNbits
+//         address payable wallet,
+//         IERC20 token
+//     )
+//         MintedCrowdsale()
+//         Crowdsale(rate, wallet, token)
+//         public
+//     {
+
+//     }
+// }
+
+// contract MyCrowdsaleDeployer {
+//     constructor()
+//         public
+//     {
+//         // create a mintable token
+//         ERC20Mintable token = new Donate();
+
+//         // create the crowdsale and tell it about the token
+//         Crowdsale crowdsale = new MyCrowdsale(
+//             1,               // rate, still in TKNbits
+//             msg.sender,      // send Ether to the deployer
+//             token            // the token
+//         );
+//         // transfer the minter role from this contract (the default)
+//         // to the crowdsale, so it can mint tokens
+//         token.addMinter(address(crowdsale));
+//         token.renounceMinter();
+
+
+//     }
+
+// 	// function getAmountRaised() public view returns (uint256){
+// 	// 	uint256 amountRaised = address(this).crowdsale.weiRaised(); 
+// 	// 	return amountRaised;
+// 	// }
+// }
--- test/Donate.js
+++ test/Donate.js
@@ -1,14 +1,55 @@
 const { expect } = require("chai");
+//import ether from './util/ether';
+const web3 = require("web3");
 
 describe("Donate contract", function() {
   it("Deployment should work", async function() {
-    const [owner] = await ethers.getSigners();
 
-    const Donate = await ethers.getContractFactory("Donation");
+    const _name = 'Dapp Token';
+    const _symbol = 'DAPP';
+    const _decimals = 18;
 
-    const donateDeployer = await Donate.deploy();
+    const [owner, addr1, addr2] = await ethers.getSigners();
+
+    const Donate = await ethers.getContractFactory("Donate");
+
+    const donateDeployer = await Donate.deploy(_name, _symbol, _decimals);
+
     await donateDeployer.deployed();
 
-   //not tests yet
+    const balance = await donateDeployer.balanceOf(owner.getAddress());
+    console.log(parseInt(balance));
+    const name = await donateDeployer.name();
+    console.log(name);
+    const symbol = await donateDeployer.symbol();
+    console.log(symbol);
+    const totalSupply = await donateDeployer.totalSupply();
+    console.log(parseInt(totalSupply));
+  
+    
+    const _rate = 500;
+    const _wallet = await owner.getAddress();
+    const _tokenAddress = await donateDeployer.address;
+
+    console.log(_tokenAddress)
+
+    const Crowdsale = await ethers.getContractFactory("DonateCrowdsale")
+    const CrowdsaleDeployer = await Crowdsale.deploy(_rate , _wallet, _tokenAddress);
+    await CrowdsaleDeployer.deployed();
+
+    const crowdsaleAddress = await CrowdsaleDeployer.address;
+
+    const weiAmount = web3.utils.toWei('1', 'ether')
+
+    await donateDeployer.transferOwnership(crowdsaleAddress);
+
+
+   
+    //await donateDeployer.connect(owner).transfer(await addr1.getAddress(), 10);
+
+    //await Crowdsale.sendTransaction({ value: weiAmount, from: addr1 });
+
+
+    
   });
 });
