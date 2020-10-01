668b7f3426e1b983af1e3ba4be770ba7f6012227
dmarzzz <danmarzec@icloud.com>
2020-10-01 15:53:35 -0400

    ethersjs wallet connect functionality

--- src/components/Topbar.tsx
+++ src/components/Topbar.tsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState } from 'react';
 import styled from "styled-components";
-//delete later
-// import Button from '@material-ui/core/Button';
+import { ethers } from "ethers";
+
+const BUIDLER_EVM_NETWORK_ID = '31337'
+const ERROR_CODE_TX_REJECTED_BY_USER = 4001
 
 const Main = styled.div`
   background-color: ${({ theme }) => theme.colors.primaryDarkColor};
@@ -47,7 +49,86 @@ const FundButton = styled.div`
   }
 `;
 
+declare const window: any;
+declare const accounts: any;
+
+const ethereum = window.ethereum;
+
 const Topbar: React.FC<{}> = () => {
+
+ //eth state
+ const [Addr, setAddr] = useState()
+ const [provider, setProvider] = useState()
+ const [TokenData, setTokenData] = useState()
+ const [networkError, setNetworkError] = useState()
+ const [txError, setTxError] = useState()
+ const [txBeingSent, setTxBeingSent] = useState()
+ const [getTestAddr, setTestAddr] = useState('1')
+ const [txHash, setTxHash] = useState(0)
+ const [associateSuccess, setAssociateSuccess] = useState()
+
+ React.useEffect(() => {
+  const windowProvider = async () => {
+      if (typeof window.ethereum !== 'undefined'
+          || (typeof window.web3 !== 'undefined')) {
+
+          setProvider(window['ethereum'] || window.web3.currentProvider);
+
+          try {
+              await provider.enable();
+          } catch (e) {
+              console.error('user refused to connect');
+          }
+          // console.log('network:', provider.networkVersion);
+          // console.log('selectedAddress:', provider.selectedAddress);
+          // console.log('is metamask:', provider.isMetaMask);
+      }
+  }
+  windowProvider();
+});
+
+if (ethereum) {
+  ethereum.on('accountsChanged', function (accounts: any) {
+      setAddr(accounts[0])
+  })
+}
+
+async function connectWallet() {
+  const tempAddr = await window.ethereum.enable()
+  setAddr(tempAddr)
+  if (!checkNetwork()) {
+      return;
+  }
+  initializeEthers()
+}
+
+async function initializeEthers() {
+  const provider3 = new ethers.providers.Web3Provider(window.ethereum)
+  setProvider(provider3)
+  // const tokenTemp = new ethers.Contract(
+  //     contractAddress.push,
+  //     PushArtifact.abi,
+  //     provider3.getSigner(0)
+  // )
+  // setTokenData(tokenTemp)
+}
+
+async function checkNetwork() {
+  if (window.ethereum.networkVersion === BUIDLER_EVM_NETWORK_ID) {
+      return true;
+  }
+  setNetworkError('Please connect Metamask to Localhost:8545')
+  return false;
+}
+
+
+// if (!Addr) {
+// //<Button variant="contained" color="primary" onClick={() => connectWallet()} > Connect Wallet</Button>
+// return <div>
+//       <h1> Connect dat wallet</h1>
+//   </div>
+// }
+
   return (
     <Main>
       <User>
@@ -56,7 +137,7 @@ const Topbar: React.FC<{}> = () => {
       </User>
       <FundButton>
         <button onClick={()=>{console.log("Test")}} >Fork Community</button>
-        <button>Boost This Server!</button>
+        {Addr ? <button>Boost This Server!</button> : <button onClick={() => connectWallet()} > Connect Wallet</button>}
       </FundButton>
     </Main>
   );
